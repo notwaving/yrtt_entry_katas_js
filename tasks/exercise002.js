62bb440506ac9ffc9629032f4437588b71af8e5e
notwaving <samanthaburke.uk@gmail.com>
2020-11-24 21:36:43 +0000

    002 passing

--- tasks/exercise002.js
+++ tasks/exercise002.js
@@ -2,10 +2,15 @@
 // Your task is to make the 'past' function return the time converted to milliseconds.
 // More examples in the test cases below.
 
-function past(h, m, s){
-    // Your code here!
+function past(h, m, s) {
+  // Multiply by number of milliseconds in hours, minutes, seconds
+  h = h * 3600000;
+  m = m * 60000;
+  s = s * 1000;
+  // Return the sum of hours, minutes, seconds
+  return h + m + s;
 }
 
 module.exports = {
-    past
+  past,
 };
