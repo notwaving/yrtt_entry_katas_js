f33e6d3b5031aed54d441a5a11cdbad83c5d7205
notwaving <samanthaburke.uk@gmail.com>
2020-11-24 17:35:47 +0000

    saves progress

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -1,16 +1,17 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 function pigLatin(str) {
-  // split the given phrase an array of words
+  // split the given phrase into an array of words
   let wordArray = str.split(' ');
   return (
-    wordArray
+		wordArray
+			// For each word in the array...
       .map(function (word) {
-        // For each word in the array, identify the first letter
+				// Identify the first letter
         let firstLetter = word.charAt(0);
 
         // Identify punctuation, remove it from the word and store in a variable
-        // ?????? let punctuation = (some regex)
+        let punctuation = punctuation.replace(//[\s|\.]+$/i, ''/)
 
         // Return each word, starting at the 2nd letter, then add the first letter, plus 'ay'
         return word.slice(1) + firstLetter + 'ay';
